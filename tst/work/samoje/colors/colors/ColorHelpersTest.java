0224f1e2cb1026887ff4cb356d7497da475fae63
Jennie Sadler <jenlersadnie@gmail.com>
2015-12-19 18:40:31 -0500

    Relocating ColorHelpers test cases to the proper test class.

--- tst/work/samoje/colors/colors/ColorHelpersTest.java
+++ tst/work/samoje/colors/colors/ColorHelpersTest.java
@@ -1,14 +1,48 @@
 package work.samoje.colors.colors;
 
-import static org.junit.Assert.fail;
-
+import org.junit.Assert;
 import org.junit.Test;
 
-public class ColorHelpersTest {
+import work.samoje.colors.ColorHelpers;
 
+public class ColorHelpersTest {
     @Test
-    public void test() {
-        fail("Not yet implemented");
+    public void test_getNearestAbsoluteValue_returns0forMidpoint() {
+        Assert.assertEquals(0, ColorHelpers.getNearestAbsoluteValue(255.0 / 2));
     }
 
+    @Test
+    public void test_getNearestAbsoluteValue_roundsDown() {
+        for (double i = 1; i < 127; i++) {
+            Assert.assertEquals(0, ColorHelpers.getNearestAbsoluteValue(i));
+        }
+    }
+
+    @Test
+    public void test_getNearestAbsoluteValue_roundsUp() {
+        for (double i = 128; i < 256; i++) {
+            Assert.assertEquals(255, ColorHelpers.getNearestAbsoluteValue(i));
+        }
+    }
+
+    @Test
+    public void test_boundByColorRange_returnsValuesInRange() {
+        for (double i = 0; i < 256; i++) {
+            Assert.assertEquals((int) i, ColorHelpers.boundByColorRange(i));
+        }
+    }
+
+    @Test
+    public void test_boundByColorRange_roundsUp() {
+        for (double i = -1; i > -255 * 2; i--) {
+            Assert.assertEquals(0, ColorHelpers.boundByColorRange(i));
+        }
+    }
+
+    @Test
+    public void test_boundByColorRange_roundsDown() {
+        for (double i = 256; i < 255 * 2; i++) {
+            Assert.assertEquals(255, ColorHelpers.boundByColorRange(i));
+        }
+    }
 }
--- tst/work/samoje/colors/grid/ColorGridTest.java
+++ tst/work/samoje/colors/grid/ColorGridTest.java
@@ -1,48 +1,5 @@
 package work.samoje.colors.grid;
 
-import org.junit.Assert;
-import org.junit.Test;
-
-import work.samoje.colors.ColorHelpers;
 
 public class ColorGridTest {
-    @Test
-    public void test_getNearestAbsoluteValue_returns0forMidpoint() {
-        Assert.assertEquals(0, ColorHelpers.getNearestAbsoluteValue(255.0 / 2));
-    }
-
-    @Test
-    public void test_getNearestAbsoluteValue_roundsDown() {
-        for (double i = 1; i < 127; i++) {
-            Assert.assertEquals(0, ColorHelpers.getNearestAbsoluteValue(i));
-        }
-    }
-
-    @Test
-    public void test_getNearestAbsoluteValue_roundsUp() {
-        for (double i = 128; i < 256; i++) {
-            Assert.assertEquals(255, ColorHelpers.getNearestAbsoluteValue(i));
-        }
-    }
-
-    @Test
-    public void test_boundByColorRange_returnsValuesInRange() {
-        for (double i = 0; i < 256; i++) {
-            Assert.assertEquals((int) i, ColorHelpers.boundByColorRange(i));
-        }
-    }
-
-    @Test
-    public void test_boundByColorRange_roundsUp() {
-        for (double i = -1; i > -255 * 2; i--) {
-            Assert.assertEquals(0, ColorHelpers.boundByColorRange(i));
-        }
-    }
-
-    @Test
-    public void test_boundByColorRange_roundsDown() {
-        for (double i = 256; i < 255 * 2; i++) {
-            Assert.assertEquals(255, ColorHelpers.boundByColorRange(i));
-        }
-    }
 }
